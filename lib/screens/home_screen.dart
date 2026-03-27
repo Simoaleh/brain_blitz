4a6353f84f65d0e5fe5ffe6c7fe187bfbc78732d
Josiiya <jashabrogar10@gmai.com>
2026-03-27 21:07:39 +0800

    add leaderboards

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:brain_blitz/widgets/menu_button.dart';
 import 'package:brain_blitz/screens/game_screen.dart';
+import 'package:brain_blitz/screens/leaderboards.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,32 +10,41 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            //Logo
-            const Text(
-              'Brain\nBlitz',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 48,
-                fontWeight: FontWeight.bold,
-                color: Colors.orange,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Logo
+              const Text(
+                'Brain\nBlitz!',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 48,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.orange,
+                ),
               ),
-            ),
-            const SizedBox(height: 40),
+              const SizedBox(height: 40),
 
-            //Menu buttons
-            MenuButton(
-              label: 'SPELLING BEE',
-              onTap: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => const GameScreen()),
+              // Menu buttons
+              MenuButton(
+                label: 'SPELLING BEE',
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const GameScreen()),
+                ),
               ),
-            ),
-            MenuButton(label: 'Mathematics', onTap: () {}),
-            MenuButton(label: 'Multiplayer', onTap: () {}),
-          ],
+              MenuButton(label: 'Mathematical', onTap: () {}),
+              MenuButton(label: 'Multiplayer', onTap: () {}),
+              MenuButton(
+                label: 'Leaderboards',
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const LeaderboardsScreen()),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );

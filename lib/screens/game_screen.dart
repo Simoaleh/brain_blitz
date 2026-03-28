dcb73ae3a7022288bd578b6932ef0f44e3475b35
Simo <ferrer102504@gmail.com>
2026-03-28 15:01:42 +0800

    still have bugs

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -29,38 +29,109 @@ class _GameScreenState extends State<GameScreen> {
       body: SafeArea(
         child: Column(
           children: [
-            const SizedBox(height: 40),
-            Stack(
-              alignment: Alignment.center,
+            // Top bar
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Image.asset(
-                  'assets/images/game_background.png',
-                  width: double.infinity,
-                  height: 300,
-                  fit: BoxFit.fill,
+                IconButton(
+                  icon: Image.asset(
+                    'assets/images/settings_icon.png',
+                    width: 64,
+                    height: 64,
+                  ),
+                  onPressed: () {},
                 ),
-                gameState.isLoading
-                    ? const CircularProgressIndicator()
-                    : Positioned(
-                        left: 60,
-                        right: 60,
-                        top: 50,
-                        bottom: 40,
-                        child: Text(
-                          gameState.currentQuestion?.definition ?? '',
-                          textAlign: TextAlign.center,
-                          softWrap: true,
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 5,
-                          style: GoogleFonts.frederickaTheGreat(
-                            fontSize: 25,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
+                Text(
+                  'LEVEL 1',
+                  style: GoogleFonts.pressStart2p(
+                    fontSize: 30,
+                    color: Colors.orange,
+                  ),
+                ),
+                IconButton(
+                  icon: Image.asset(
+                    'assets/images/hint_icon.png',
+                    width: 64,
+                    height: 64,
+                  ),
+                  onPressed: () {},
+                ),
               ],
             ),
-            // letter tiles ...
+            SizedBox(
+              height: 360,
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  // Chalkboard
+                  Column(
+                    children: [
+                      Image.asset(
+                        'assets/images/game_background.png',
+                        width: double.infinity,
+                        height: 260,
+                        fit: BoxFit.fill,
+                      ),
+                      Image.asset(
+                        'assets/images/wood.jpg',
+                        width: double.infinity,
+                        height: 100,
+                        fit: BoxFit.fill,
+                      ),
+                    ],
+                  ),
+                  // Definition text on chalkboard
+                  Positioned(
+                    left: 60,
+                    right: 60,
+                    top: 40,
+                    height: 180,
+                    child: gameState.isLoading
+                        ? const Center(child: CircularProgressIndicator())
+                        : Text(
+                            gameState.currentQuestion?.definition ?? '',
+                            textAlign: TextAlign.center,
+                            softWrap: true,
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 5,
+                            style: GoogleFonts.frederickaTheGreat(
+                              fontSize: 24,
+                              color: Colors.white,
+                            ),
+                          ),
+                  ),
+                  // Wizard overlapping chalkboard bottom-left
+                  Positioned(
+                    bottom: 0,
+                    left: 20,
+                    child: Image.asset(
+                      'assets/images/regular_wiz.png',
+                      width: 120,
+                      height: 200,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 16),
+
+            // Letter tiles
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                gameState.currentQuestion?.word.length ?? 0,
+                (_) => Container(
+                  width: 36,
+                  height: 36,
+                  margin: const EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(4),
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
